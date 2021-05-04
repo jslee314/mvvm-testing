32144ab719b319e11c29b51432e956e1aa8ed069
jslee314 <jslee_314@naver.com>
2021-05-04 22:21:41 +0900

    databinding 설정

--- app/src/main/java/com/jslee/retrofittesting/MainActivity.kt
+++ app/src/main/java/com/jslee/retrofittesting/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.jslee.retrofittesting
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.databinding.DataBindingUtil
 import com.jslee.retrofittesting.databinding.ActivityMainBinding
 
@@ -9,7 +12,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private val user: User = User("jslee314")
+    private val user: User = User("jslee")
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +22,25 @@ class MainActivity : AppCompatActivity() {
 
         binding.user = user
 
+        binding.doneButton.setOnClickListener{
+            addName(it)
+        }
 
     }
 
+    private fun addName(view: View){
+        binding.apply {
+            user?.age = ageEditText.text.toString()
+
+            invalidateAll() // data가 변한 후 연결된 view들에 변화를 알려주는 함수
+            ageEditText.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            ageText.visibility = View.VISIBLE
+        }
+
+        // 키보드 숨기기
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+
+    }
 }
---
+++ app/src/main/java/com/jslee/retrofittesting/User.kt
@@ -0,0 +1,4 @@
+package com.jslee.retrofittesting
+
+class User (var name: String = "", var age: String = ""){
+}
