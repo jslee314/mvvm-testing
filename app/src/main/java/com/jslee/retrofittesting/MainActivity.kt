86dbb524a1fd8c98af6d9d69080343715cc17ab7
jslee314 <jslee_314@naver.com>
2021-05-05 00:09:11 +0900

    navigation 연결하기

--- app/src/main/java/com/jslee/retrofittesting/MainActivity.kt
+++ app/src/main/java/com/jslee/retrofittesting/MainActivity.kt
@@ -20,25 +20,5 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.user = user
 
-//        binding.doneButton.setOnClickListener{
-//            addName(it)
-//        }
-
     }
-
-//    private fun addName(view: View){
-//        binding.apply {
-//            user?.age = ageEditText.text.toString()
-//
-//            invalidateAll() // data가 변한 후 연결된 view들에 변화를 알려주는 함수
-//            ageEditText.visibility = View.GONE
-//            doneButton.visibility = View.GONE
-//            ageText.visibility = View.VISIBLE
-//        }
-//
-//        // 키보드 숨기기
-//        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-//        imm.hideSoftInputFromWindow(view.windowToken, 0)
-//
-//    }
 }
