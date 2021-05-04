3d1068132047320c8e181b622eada4f88381216c
jslee314 <jslee_314@naver.com>
2021-05-04 21:58:46 +0900

    styles 설정

--- app/src/main/java/com/jslee/retrofittesting/MainActivity.kt
+++ app/src/main/java/com/jslee/retrofittesting/MainActivity.kt
@@ -2,10 +2,24 @@ package com.jslee.retrofittesting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.jslee.retrofittesting.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val user: User = User("jslee314")
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.user = user
+
+
     }
+
 }
