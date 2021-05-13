c25eeb94e302350eb8682e932400ee7c01548c0d
jslee314 <jslee_314@naver.com>
2021-05-13 15:18:17 +0900

    백그라운드 터치시 키패트 숨기기

--- app/src/main/java/com/jslee/retrofittesting/MainActivity.kt
+++ app/src/main/java/com/jslee/retrofittesting/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.jslee.retrofittesting
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
@@ -33,5 +36,15 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.myNavHostFragment)
         return NavigationUI.navigateUp(navController, binding.drawerLayout)
+    }
+
+    /**
+     * 백그라운드 터치시 키패트 숨기기     */
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        if (currentFocus != null) {
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
+        }
+        return super.dispatchTouchEvent(ev)
     }
 }
