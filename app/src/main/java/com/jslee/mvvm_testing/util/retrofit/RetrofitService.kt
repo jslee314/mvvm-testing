5bd2c494b2825dc2aae64c95bfe151982989c245
jslee314 <jslee_314@naver.com>
2021-05-14 15:21:27 +0900

    query parameter (option(filter)) 조건에 따라 RecyclerView에 값 뿌려주기 (필터링)

--- app/src/main/java/com/jslee/mvvm_testing/util/retrofit/RetrofitService.kt
+++ app/src/main/java/com/jslee/mvvm_testing/util/retrofit/RetrofitService.kt
@@ -4,10 +4,28 @@ import com.jslee.mvvm_testing.data.remote.MarsProperty
 import retrofit2.http.GET
 import retrofit2.http.Query
 
+
+/**
+ * https://android-kotlin-fun-mars-server.appspot.com/realestate
+ * > [{"price":450000,"id":"424905","type":"buy","img_src":"http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000MR0044631300503690E01_DXXX.jpg"},
+ * {"price":8000000,"id":"424906","type":"rent","img_src":"http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000ML0044631300305227E03_DXXX.jpg"},
+ * {"price":11000000,"id":"424907","type":"rent","img_src":"http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000MR0044631290503689E01_DXXX.jpg"},
+ * {"price":8000000,"id":"424908","type":"rent","img_src":"http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000ML0044631290305226E03_DXXX.jpg"},
+ * {"price":12000000,"id":"424909","type":"rent","img_src":"http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000MR0044631280503688E0B_DXXX.jpg"},
+ * {"price":500000,"id":"424910","type":"buy","img_src":"http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000ML0044631280305225E03_DXXX.jpg"},
+ * {"price":450000,"id":"424911","type":"buy","img_src":"http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000MR0044631270503687E03_DXXX.jpg"},
+ *
+ *
+ *
+ */
+
+
+
 interface RetrofitService {
     /**
      * Coroutine 범위에있는 경우(suspend 함수이므로..)
-     * await()로 가져올 수있는 [MarsProperty]의 Coroutine [List]를 반환합니다.*/
+     * await()로 가져올 수있는 [MarsProperty]의 Coroutine [List]를 반환합니다.
+     * https://android-kotlin-fun-mars-server.appspot.com/realestate*/
     @GET("realestate")
     suspend fun getProperties(): List<MarsProperty>
 
