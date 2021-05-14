682345da08e8a82da59920b87709eb59974e5c11
jslee314 <jslee_314@naver.com>
2021-05-15 00:19:47 +0900

    코드정리

--- app/src/main/java/com/jslee/mvvm_testing/data/remote/MarsProperty.kt
+++ app/src/main/java/com/jslee/mvvm_testing/data/remote/MarsProperty.kt
@@ -11,7 +11,7 @@ import kotlinx.android.parcel.Parcelize
 * @버젼 : 0.0.0
 * @최초작성일 : 2021-05-13 오후 1:40
 * @작성자 : 이재선
-**/
+
 
 // 응답 샘플이 다음과 같기때문에 이를 기반으로 data 클래스를 만들자
 //[{"price":450000,
@@ -19,13 +19,17 @@ import kotlinx.android.parcel.Parcelize
 //    "type":"rent",
 //    "img_src":"http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000ML0044631300305227E03_DXXX.jpg"},
 //...]
-
+ **/
 @Parcelize
 data class MarsProperty (
+
         val id: String,
+
         @Json(name = "img_src")
         val imgSrcUrl: String, // 우리 클래스에서 img_src를 JSON에서 imgSrcUrl로 매핑하는 데 사용
+
         val type: String,
+
         val price: Double) : Parcelable {
 
     val isRental
--- app/src/main/java/com/jslee/mvvm_testing/data/remote/RemoteImg.kt
+++ app/src/main/java/com/jslee/mvvm_testing/data/remote/RemoteImg.kt
@@ -4,18 +4,15 @@ import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class RemoteImg(val imgSrcUrl: String, val type:String, val price:Double) : Parcelable {
+data class RemoteImg(
+
+    val imgSrcUrl: String,
+
+    val type:String,
+
+    val price:Double) : Parcelable {
 
     val isRental
         get() = type == "rent"
 
-
 }
-
-//data class RemoteImg(val id:String, @Json(name = "img_src") val imgSrcUrl: String, val type:String, val price:Double) : Parcelable {
-//
-//    val isRental
-//        get() = type == "rent"
-//
-//
-//}
