65a1905d686006b26a79f625ac08d99a2125a329
jslee314 <jslee_314@naver.com>
2021-05-26 17:40:43 +0900

    di 수정

--- app/src/main/java/com/jslee/mvvm_testing/di/AppComponent.kt
+++ app/src/main/java/com/jslee/mvvm_testing/di/AppComponent.kt
@@ -1,13 +1,12 @@
 package com.jslee.mvvm_testing.di
 
 import android.content.Context
-import com.jslee.mvvm_testing.data.AppRepository
 import com.jslee.mvvm_testing.home.HomeFragment
 import com.jslee.mvvm_testing.home.HomeModule
 import com.jslee.mvvm_testing.quiz.QuizFragment
 import com.jslee.mvvm_testing.quiz.QuizModule
-import com.jslee.mvvm_testing.rslt_network.NetworkFragment
-import com.jslee.mvvm_testing.rslt_network.NetworkModule
+import com.jslee.mvvm_testing.ground.NetworkFragment
+import com.jslee.mvvm_testing.ground.NetworkModule
 import com.jslee.mvvm_testing.rslt_room.RoomFragment
 import com.jslee.mvvm_testing.video.VideoFragment
 import com.jslee.mvvm_testing.video.VideoModule
@@ -17,35 +16,52 @@ import javax.inject.Singleton
 
 /**
  * Main component for the application.
- * See the `TestApplicationComponent` used in UI tests.
+ * See the 'TestApplicationComponent' used in UI tests.
+ *
+ * @Component는 interface 또는 abstract 클래스에 붙일수 있습니다.
+ * 컴파일 타임에 애노테이션 프로세서에 의해 생성된 클래스는 접두어 ‘Dagger’와 @Component가 붙은 클래스이름이 합쳐진 형식의 이름을 갖습니다.
+ * 예를들면 @Component interface MyComponent{ … }란 인터페이스가 있다면 DaggerMyComponent라는 클래스가 생성되게 됩니다.
+ *
+ * 컴포넌트 메소드에는 Provision 메소드와 Member-Injection 메소드가 있습니다.
+ * Provision Method
+ * :프로비전 메소드에는 매개변수가 없고, 모듈이 제공하는 객체의 타입을 반환형으로 갖습니다.
+ * 생성된 컴포넌트 클래스에서 이 메소드를 이용하여 객체를 얻을 수 있습니다.
+ *
+ * Member-Injection Method
+ * 의존성을 주입시킬 객체를 메소드의 파라미터로 넘기는 방법입니다.
+ * 멤버인젝션 메소드를 호출 하게 되면 타겟 클래스 내의 @Inject 필드에 객체를 주입받게 됩니다.
+ *
  */
 @Singleton
 @Component(
     modules = [
-        AppModule::class,
-        ViewModelBuilderModule::class,
+        AppModule::class, // RemoteDataSource, LocalDataSource, AppDatabase, CoroutineDispatcher
+        ViewModelBuilderModule::class, // ViewModelProvider.Factory
 
-        HomeModule::class,
+        HomeModule::class, // HomeViewModel -> ViewModel
         QuizModule::class,
         NetworkModule::class,
         VideoModule::class
-    ]
-)
+    ])
 interface AppComponent {
 
+    // Provision Method : Injection 시킬 객체를 넘기는 Method
     @Component.Factory
     interface Factory {
         fun create(@BindsInstance applicationContext: Context): AppComponent
     }
 
+    // Member-Injection Method : 멤버 파라미터에 의존성 주입 시킬 객체를 넘기는 Method
+    //                             인자로 받은 HomeFragment 내부 멤버필드에 의존성 주입
     fun inject(fragment: HomeFragment)
     fun inject(fragment: QuizFragment)
     fun inject(fragment: RoomFragment)
     fun inject(fragment: NetworkFragment)
     fun inject(fragment: VideoFragment)
 
-    val repository: AppRepository
+//    val repository: AppRepository
 }
+
 /**
  *  팩토리 패턴
  *  클라이언트 코드로부터 서브 클래스의 인스턴스화를 제거하여 서로 간의 종속성을 낮추고,
--- app/src/main/java/com/jslee/mvvm_testing/di/AppModule.kt
+++ app/src/main/java/com/jslee/mvvm_testing/di/AppModule.kt
@@ -14,9 +14,23 @@ import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import javax.inject.Singleton
 
+
+/**
+ * @Module 어노태이션을 붙인 AppModule 클래스는 App을 만들기 위해 필요한 모든 객체를 얻을 수 있다.
+ * 타 클래스에 의존성이 있는 클래스들을 제공해주는(provide~) 클래스를 정의해주는데 쓰임
+ *
+ * @Provides 어노테이션을 붙인 각 함수를 Component 클래스에게 제공해줌
+ *
+ * Component에 연결되어 의존성 객체를 생성하는 역할입니다. 생성 후 Scope에 따라 객체를 관리도 합니다
+ * @Module은 클래스에만 붙이고, @Provides는 반드시 @Module 클래스에 선언된 메서드에만 사용합니다
+ * Module 클래스는 의존성 주입에 필요한 객체들을 @Provide @Binds 메서드를 통해 관리합니다.
+ * @Provides @Binds메서드의 파라미터 또한 Component에게 객체를 전달받고 Component에게 제공합니다
+ */
 @Module
 object AppModule {
 
+    /** 필요한 인자(AppDatabase, CoroutineDispatcher)를 Component로 부터 전달받아
+        RemoteDataSource 객체를 생성해서 Component에게 제공  */
     @JvmStatic
     @Singleton
     @RemoteDataSourceAnnotation
@@ -30,6 +44,8 @@ object AppModule {
         )
     }
 
+    /** 필요한 인자(AppDatabase, CoroutineDispatcher)를 Component로 부터 전달받아
+        LocalDataSource 객체를 생성해서 Component에게 제공  */
     @JvmStatic
     @Singleton
     @LocalDataSourceAnnotation
@@ -39,10 +55,12 @@ object AppModule {
         ioDispatcher: CoroutineDispatcher
     ): AppDataSource {
         return LocalDataSource(
-            database.userDao(), database.scoreDao(), database.videoDao(),  ioDispatcher
+            database.userDao(), database.scoreDao(), database.videoDao(), ioDispatcher
         )
     }
 
+    /** 필요한 인자(Context)를 Component로 부터 전달받아
+        AppDatabase 객체를 생성해서 Component에게 제공  */
     @JvmStatic
     @Singleton
     @Provides
@@ -54,6 +72,7 @@ object AppModule {
         ).build()
     }
 
+    /** CoroutineDispatcher 객체(인스턴스)를 생성해서 Component에게 제공  */
     @JvmStatic
     @Singleton
     @Provides
